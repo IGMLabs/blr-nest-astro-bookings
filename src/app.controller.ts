b6d22f6a9a63195609691259bed9773728aa1702
Bruno Lopez <bruno.lopez.rodriguez.es@gmail.com>
2022-06-27 12:34:02 +0200

    feat : body post put

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+} from "@nestjs/common";
 import { AppService } from "./app.service";
+import { Client } from "./client.interface";
 
 @Controller()
 export class AppController {
@@ -8,5 +20,105 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/test")
+  public getTest(): string {
+    return "Hola Test";
+  }
+
+  @Get("/param/:id")
+  public getParam(@Param("id") id: string): string {
+    const type = typeof id;
+    return `Param: ${id} of type ${type}`;
+  }
+
+  @Get("/square/:someParam")
+  public getSquare(@Param("someParam") someParam: number): string {
+    const type = typeof someParam;
+    const square = someParam * someParam;
+    return `Square of ${someParam} of type ${type} is ${square}`;
+  }
+
+  @Get("/square/NaN/:someParam")
+  public getSquareNaN(@Param("someParam") someParam: number): string {
+    const someNumber = parseInt(someParam.toString());
+    if (isNaN(someNumber)) {
+      throw new HttpException(`${someParam} is not a number`, HttpStatus.BAD_REQUEST);
+    }
+    const type = typeof someNumber;
+    const square = someNumber * someNumber;
+    return `Square of ${someNumber} of type ${type} is ${square}`;
+  }
+
+  @Get("/square/pipe/:someParam")
+  public getSquarePipe(@Param("someParam", ParseIntPipe) someNumber: number): string {
+    const type = typeof someNumber;
+    const square = someNumber * someNumber;
+    return `Square of ${someNumber} of type ${type} is ${square}`;
+  }
+
+  @Get("/multiply/:someNumber/:otherNumber")
+  public getMultiply(
+    @Param("someNumber", ParseIntPipe) someNumber: number,
+    @Param("otherNumber", ParseIntPipe) otherNumber: number,
+  ): number {
+    const multiply = someNumber * otherNumber;
+    return multiply;
+  }
+
+  @Get("/multiply/query")
+  public getMultiplyQuery(
+    @Query("a", ParseIntPipe) someNumber: number,
+    @Query("b", ParseIntPipe) otherNumber: number,
+  ): number {
+    return this.appService.multiply(someNumber, otherNumber);
+  }
+
+  @Get("/division/query")
+  public getDivisionQuery(
+    @Query("a", ParseIntPipe) someNumber: number,
+    @Query("b", ParseIntPipe) otherNumber: number,
+  ): number {
+    if (someNumber == 0 || otherNumber == 0) {
+      throw new HttpException(`${someNumber} and/or ${otherNumber} are under 1`, HttpStatus.BAD_REQUEST);
+    }
+    return this.appService.division(someNumber, otherNumber);
+  }
+
+  @Get("/squareRoot/query")
+  public getSquareRootQuery(@Query("a", ParseIntPipe) someNumber: number): number {
+    if (someNumber < 1) {
+      throw new HttpException(`${someNumber} is under 1`, HttpStatus.BAD_REQUEST);
+    }
+    return this.appService.squareRoot(someNumber);
+  }
+
+  @Post("")
+  public postHello(@Body() nombre: { name: string }): string {
+    const type = typeof nombre;
+    const nameString = JSON.stringify(nombre);
+    return `${nombre} of type ${type} but is a ${nameString}`;
+  }
+
+  @Post("name")
+  public postHelloName(@Body() payload: { name: string }): string {
+    return `Hello ${payload.name}`;
+  }
+
+  @Post("client")
+  public postClient(@Body() payload: Client): Client {
+    return this.appService.saveClient(payload);
+  }
+
+  @Put("client/:id")
+  public putClient(@Param("id") clientId: string, @Body() payload: Client): Client {
+    try {
+      return this.appService.saveClient(payload);
+    } catch (error) {
+      const message: string = error.message;
+      if (message.startsWith("Not found:")) throw new HttpException(message, HttpStatus.NOT_FOUND);
+      else throw new HttpException(message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from "@nestjs/common";
+import { Client } from "./client.interface";
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return "Hello World!";
+  }
+
+  public multiply(someNumber: number, otherNumber: number): number {
+    const multiply = someNumber * otherNumber;
+    return multiply;
+  }
+
+  public division(someNumber: number, otherNumber: number): number {
+    const division = someNumber / otherNumber;
+    return division;
+  }
+
+  public squareRoot(someNumber: number): number {
+    const sqrt = Math.sqrt(someNumber);
+    return sqrt;
+  }
+
+  public saveClient(client: Client): Client {
+    client.id = Math.random().toString();
+    return client;
+  }
+
+  public updateClient(clientId: string, client: Client): Client {
+    if (clientId !== "") {
+      throw new Error("Not found: " + clientId);
+    }
+    return client;
   }
 }
